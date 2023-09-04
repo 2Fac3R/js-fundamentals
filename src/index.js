ec4986dcda27c1b82d1ce685ed9f55ddc3846f83
Gustavo Navarro <genr1818@gmail.com>
2023-09-04 16:23:49 -0600

    more explicit names

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@
 
 // Arrays creation
 const characters = [
-  { id: 1, name: 'Goku', anime: 'Dragon Ball', characteristics: ['sayajín', 'comelón'], sayPhrase: () => 'No soy kakaroto' },
-  { id: 2, name: 'Robin', anime: 'One Piece', characteristics: ['misteriosa', 'inteligente'], sayPhrase: () => 'Gigantesco mano' },
-  { id: 3, name: 'Orihime', anime: 'Bleach', characteristics: ['llorona'], sayPhrase: () => 'wua wua' },
-  { id: 4, name: 'Luffy', anime: 'One Piece', characteristics: ['comelón', 'ambisioso', 'libre'], sayPhrase: () => 'kaizoku ni orewa naru' },
-  { id: 5, name: 'Shikamaru', anime: 'Naruto', characteristics: ['inteligente', 'perezoso'], sayPhrase: () => 'Que flojera!' },
-  { id: 6, name: 'Ash', anime: 'Pokemon', characteristics: ['joven', 'resiliente'], sayPhrase: () => 'Tengo que ser siempre el mejor...' },
-  { id: 7, name: 'Sakura', anime: 'Sakura Card Captor', characteristics: [/* ni idea */], sayPhrase: () => 'Ni idea...' },
-  { id: 8, name: 'Sakura', anime: 'Naruto', characteristics: ['fuerte', 'terca', 'llorona'], sayPhrase: () => 'Sasuke-kun' },
+  { id: 1, name: 'Goku', serie: 'Dragon Ball', characteristics: ['sayajín', 'comelón'], speak: () => 'No soy kakaroto' },
+  { id: 2, name: 'Robin', serie: 'One Piece', characteristics: ['misteriosa', 'inteligente'], speak: () => 'Gigantesco mano' },
+  { id: 3, name: 'Orihime', serie: 'Bleach', characteristics: ['llorona'], speak: () => 'wua wua' },
+  { id: 4, name: 'Luffy', serie: 'One Piece', characteristics: ['comelón', 'ambisioso', 'libre'], speak: () => 'kaizoku ni orewa naru' },
+  { id: 5, name: 'Shikamaru', serie: 'Naruto', characteristics: ['inteligente', 'perezoso'], speak: () => 'Que flojera!' },
+  { id: 6, name: 'Ash', serie: 'Pokemon', characteristics: ['joven', 'resiliente'], speak: () => 'Tengo que ser siempre el mejor...' },
+  { id: 7, name: 'Sakura', serie: 'Sakura Card Captor', characteristics: [/* ni idea */], speak: () => 'Ni idea...' },
+  { id: 8, name: 'Sakura', serie: 'Naruto', characteristics: ['fuerte', 'terca', 'llorona'], speak: () => 'Sasuke-kun' },
 ]
 
 const fruits = ['Apple', 'Orange', 'Banana', 'Watermelon', 'Mango']
@@ -29,8 +29,8 @@ console.log(reversedVegetables) // Reverse the array
 
 console.log(characters[0]) // Accesing the first element
 console.log(characters[characters.length - 1]) // Accesing the last element
-console.log(characters[3].sayPhrase())
-console.log(characters[2].anime) // Accessing array with property name
+console.log(characters[3].speak())
+console.log(characters[2].serie) // Accessing array with property name
 
 // Converting an array to a String
 console.log(typeof fruits) // Arrays are objects
@@ -47,18 +47,18 @@ characters.forEach((element, index) => console.log(element.name))
 let characterNames = characters.forEach((element, index) => element.name);
 console.log(characterNames) // undefined
 
-characters.map((element, index) => console.log(element.anime))
-let characterNameAndAnime = characters.map((element, index) => [element.name, element.anime])
-console.log(characterNameAndAnime) // new array
+characters.map((element, index) => console.log(element.serie))
+let characterNameAndserie = characters.map((element, index) => [element.name, element.serie])
+console.log(characterNameAndserie) // new array
 
 
 // Adding elements to arrays
 
-characterNameAndAnime[1] = ['Sakura', 'Naruto']
-characterNameAndAnime[10] = ['Sasuke', 'Naruto'] // This could create empty items
-characterNameAndAnime.push(['Chopper', 'One Piece'])
-console.log(characterNameAndAnime)
-console.log(characterNameAndAnime[7]) // Empty item -> undefined
+characterNameAndserie[1] = ['Sakura', 'Naruto']
+characterNameAndserie[10] = ['Sasuke', 'Naruto'] // This could create empty items
+characterNameAndserie.push(['Chopper', 'One Piece'])
+console.log(characterNameAndserie)
+console.log(characterNameAndserie[7]) // Empty item -> undefined
 
 
 // Array Methods
@@ -134,19 +134,19 @@ console.log(characters.includes(firstSakura))
 
 // The ...spread operator: expands an iterable (like an array) into more elements
 const myTVCharacters = [
-  { id: 9, name: 'Bender', anime: 'Futurama', characteristics: ['divertido', 'borracho', 'robot'], sayPhrase: () => 'Bender is god' },
-  { id: 10, name: 'Bart', anime: 'Los Simpsons', characteristics: ['travieso', 'divertido'], sayPhrase: () => 'Ya estas viejo homero' },
-  { id: 11, name: 'Caesar', anime: 'One Piece', characteristics: ['genio incomprendido'], sayPhrase: () => 'Shurorororo!' },
-  { id: 12, name: 'Don Cangrejo', anime: 'Bob Esponja', characteristics: ['avaricioso'], sayPhrase: () => 'Dame el dinero' },
-  { id: 13, name: 'Giru', anime: 'Dragon Ball GT', characteristics: ['miedoso', 'robot'], sayPhrase: () => 'Giru giru giru' },
-  { id: 14, name: 'Freezer', anime: 'Dragon Ball Z', characteristics: ['peligroso', 'inmortal', 'violento'], sayPhrase: () => 'Malditos sayayins!' },
+  { id: 9, name: 'Bender', serie: 'Futurama', characteristics: ['divertido', 'borracho', 'robot'], speak: () => 'Bender is god' },
+  { id: 10, name: 'Bart', serie: 'Los Simpsons', characteristics: ['travieso', 'divertido'], speak: () => 'Ya estas viejo homero' },
+  { id: 11, name: 'Caesar', serie: 'One Piece', characteristics: ['genio incomprendido'], speak: () => 'Shurorororo!' },
+  { id: 12, name: 'Don Cangrejo', serie: 'Bob Esponja', characteristics: ['avaricioso'], speak: () => 'Dame el dinero' },
+  { id: 13, name: 'Giru', serie: 'Dragon Ball GT', characteristics: ['miedoso', 'robot'], speak: () => 'Giru giru giru' },
+  { id: 14, name: 'Freezer', serie: 'Dragon Ball Z', characteristics: ['peligroso', 'inmortal', 'violento'], speak: () => 'Malditos sayayins!' },
 ]
 const allCharacters = [...characters, ...myTVCharacters]
 console.log(allCharacters)
 
 
 const searchRobots = allCharacters.filter(character => character.characteristics.includes('robot'))
-const searchDragonBall = allCharacters.filter(character => character.anime.includes('Dragon Ball'))
+const searchDragonBall = allCharacters.filter(character => character.serie.includes('Dragon Ball'))
 
-searchRobots.map((character) => console.log(character.sayPhrase()))
+searchRobots.map((character) => console.log(character.speak()))
 searchDragonBall.map((character) => console.log(character.name))
